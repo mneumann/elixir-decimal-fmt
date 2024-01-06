887730e02bcb7ef75cc70b2d72c76f85c7ac4643
Michael Neumann <mneumann@ntecs.de>
2024-01-06 07:04:19 +0100

    Project skeleton

---
+++ lib/decimal_fmt.ex
@@ -0,0 +1,18 @@
+defmodule DecimalFmt do
+  @moduledoc """
+  Documentation for `DecimalFmt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DecimalFmt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/decimal_fmt_test.exs
@@ -0,0 +1,8 @@
+defmodule DecimalFmtTest do
+  use ExUnit.Case
+  doctest DecimalFmt
+
+  test "greets the world" do
+    assert DecimalFmt.hello() == :world
+  end
+end
